6e823750f898a4170af0405089279f2d51b43049
SaidaElamin <saidaelameen@hotmail.com>
2017-03-24 18:10:01 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,115 +1,50 @@
+library(reshape2)
 
-#Question 1
+filename <- "getdata_dataset.zip"
 
-#Point to file for download
-
-
-setInternet2(use = TRUE) # use to let me download
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv" #define location of download file
-
-download.file(fileUrl, destfile = "idaho_housing.csv") 
-
-#read file into dataframe
-df <- read.csv("idaho_housing.csv",header = TRUE,quote = "\"'",dec = ".")
-
-#filter on just the field i want to split
-
-df_wgtp = df$wgtp
-#names(df)
-#head(df)
-splitNames = strsplit(names(df),"wgtp")
-
-#Answer, assuming that there is no "." after "wgtp"
-#splitNames[123]
-
-
-#Question 2
-
-#Load in GDP dataset
-fileUrl2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv" #define location of download file
-
-download.file(fileUrl2, destfile = "GDP_Data.csv") 
-
-#read file into dataframe with some tidy up
-df2 <- read.csv("GDP_Data.csv",header = TRUE,dec = ".",skip=4, nrows = 190)#191
-
-
-#convert field with commas to number
-#df2$X.4
-
-col2cvt <- c('X.4')  # columns for conversion
-for (i in col2cvt){
-  df2[[i]] <- as.numeric(gsub(",", "", df2[[i]]))
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
 }
 
-#Answer
-mean(df2$X.4)
+# Load activity labels + features
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activityLabels[,2] <- as.character(activityLabels[,2])
+features <- read.table("UCI HAR Dataset/features.txt")
+features[,2] <- as.character(features[,2])
+
+# Extract only the data on mean and standard deviation
+featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
+featuresWanted.names <- features[featuresWanted,2]
+featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
+featuresWanted.names = gsub('-std', 'Std', featuresWanted.names)
+featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
 
 
-#Question 3 - will try out some of the answers
+# Load the datasets
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
+trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
 
-grep("^United",df2$X.3), 3 #Think this is most right but don't understand ,3
-grep("United$",df2$X.3), 3 #wrong
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresWanted]
+testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
 
-grep("^United",df2$X.3), 4
-grep("*United",df2$X.3), 5
+# merge datasets and add labels
+allData <- rbind(train, test)
+colnames(allData) <- c("subject", "activity", featuresWanted.names)
 
+# turn activities & subjects into factors
+allData$activity <- factor(allData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+allData$subject <- as.factor(allData$subject)
 
-#Question 4
+allData.melted <- melt(allData, id = c("subject", "activity"))
+allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
 
-#Load data
-
-https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv 
-
-fileUrl4 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv" #define location of download file
-
-download.file(fileUrl4, destfile = "GDP190.csv") 
-
-#read file into dataframe
-df4 <- read.csv("GDP190.csv",header = TRUE,dec = ".",skip=4, nrows = 190)#191
-#head(df4)
-
-#load 2nd dataset
-
-
-fileUrl5 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv" #define location of download file
-
-download.file(fileUrl5, destfile = "FED_Stats.csv") 
-
-#read file into dataframe
-df5 <- read.csv("FED_Stats.csv",header = TRUE,dec = ".")
-#tail(df5)
-
-
-#Join 2 datasets
-m1 <- merge(df4, df5, by.x = "X", by.y = "CountryCode")
-
-#head(m1$Special.Notes)
-
-length(grep("*Fiscal year end: June",m1$Special.Notes))
-
-
-#Question 5
-
-#Use source code
-#Install  librarys
-install.packages("quantmod")
-
-library(quantmod)
-amzn = getSymbols("AMZN",auto.assign=FALSE)
-sampleTimes = index(amzn)
-
-
-
-colnames(amzn)
-class(amzn$AMZN.Open)
-head(amzn$AMZN.Open)
-
-substr(amzn$AMZN.Open,1,10) 
-strtrim(amzn$AMZN.Open, 10)
-format(amzn$AMZN.Open,"%a %b %d")
-length(grep("*2012",amzn$AMZN.Open))
-length(grep("*2012",sampleTimes))
-
-
-head(sampleTimes)
+write.table(allData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
